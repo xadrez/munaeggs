d0e7100504ea481c67d45f4e78bd3529910700fb
Humberto <humbsb2013@gmail.com>
2014-12-15 21:18:40 +0200

    Add map to the app, including one marker

--- _/js/myScript.js
+++ _/js/myScript.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
-console.log('initiate');
+//console.log('initiated');
+var init = function () {
+	var mapDiv = document.getElementById('map');
+	/*
+	*create a variable that stores the default position coordinates
+	*/
+	var position = new google.maps.LatLng(60.221920, 	24.805534);
+	/*
+	*Default options
+	*/
+	var options = {
+	  'center': position,
+	  'zoom': 14,
+	  'mapTypeId': google.maps.MapTypeId.ROADMAP
+	};
+	/*
+	* map is the new instance with the default values
+	*/
+	var markers =[];
+	var map = new google.maps.Map(mapDiv, options);
+	var marker = new google.maps.Marker({position:position,map:map,title:'Not draggable'});
+}
+init();
 
 });
