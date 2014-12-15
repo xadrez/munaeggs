418b0bb8618dc9ba1c0e39f10f21cdf1b1de808d
Humberto <humbsb2013@gmail.com>
2014-12-15 21:47:41 +0200

    Add markers to map and retrieve formated address

--- _/js/myScript.js
+++ _/js/myScript.js
@@ -6,6 +6,7 @@ var init = function () {
 	*create a variable that stores the default position coordinates
 	*/
 	var position = new google.maps.LatLng(60.221920, 	24.805534);
+	
 	/*
 	*Default options
 	*/
@@ -14,12 +15,47 @@ var init = function () {
 	  'zoom': 14,
 	  'mapTypeId': google.maps.MapTypeId.ROADMAP
 	};
+	
 	/*
 	* map is the new instance with the default values
 	*/
 	var markers =[];
 	var map = new google.maps.Map(mapDiv, options);
 	var marker = new google.maps.Marker({position:position,map:map,title:'Not draggable'});
+	
+	/*
+	*Add event to map. Marker
+	*/
+	
+			  google.maps.event.addListener(map, 'click', function(event) {
+			    placeMarker(event.latLng);
+//		        console.log(event.latLng);
+			    var key = 'AIzaSyBoOhXb6Gcmi26DMuhYrw9IM9tEFmZwOKk';
+			    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+event.latLng.k+","+event.latLng.D+"&key="+key+"";
+			    $.getJSON(url, function (data) {
+//			    console.log(url);
+			    	$('.widget-body').append('<p>'+data.results[0].formatted_address+'&nbsp;&nbsp;<a href="#">Remove</a></p>');
+			    
+			    });
+			   // console.log(markers);
+
+			  });
+			
+			function placeMarker(location) {
+			  var marker = new google.maps.Marker({
+			      position: location,
+			      map: map,
+			      clickable:true,
+			      draggable:true
+			  });
+			  	markers.push(marker);
+			  }
+			 
+			  	  google.maps.event.addListener(marker, 'dragend', function(event) {
+			  	alert('Drag endded');
+			  });//end of marker listener draend
+			  
+	
 }
 init();
 
